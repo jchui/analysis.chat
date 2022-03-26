72e8fbcd7398a50aba2f435f20ab754cc19b5fd7
Jeremy Chui <jeremy@jchui.me>
2022-03-26 21:55:41 +0000

    Added bulma css

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,20 @@ const IndexPage = () => {
     <main>
       <title>Home Page</title>
       <h1>Hello world</h1>
+
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+            <h2 className="title is-2">Level 2 heading</h2>
+            <p className="content">Cool content. Using Bulma!</p>
+          </div>
+          <div className="column is-four-fifths">
+            <h2 className="title is-2">Level 2 heading</h2>
+            <p className="content">This column is cool too!</p>
+          </div>
+        </div>
+      </div>
+
     </main>
   );
 };
