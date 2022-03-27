d53fd26ece3ea3431a0e70574bafc9975bce7797
Jeremy Chui <jeremy@jchui.me>
2022-03-27 00:36:41 +0000

    Added drag and drop functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,49 @@
 import * as React from 'react';
+import { useMemo, useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
 
 import '../styles/style.scss';
 
 const IndexPage = () => {
+  const onDrop = useCallback(acceptedFiles => {
+    acceptedFiles.forEach(file => {
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log('file reading was aborted');
+      reader.onerror = () => console.log('file reading has failed');
+      reader.onload = () => {
+        const binaryStr = reader.result;
+        console.log(binaryStr);
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  }, []);
+
+  const { getRootProps, getInputProps, isFocused, isDragAccept, isDragReject } =
+    useDropzone({ onDrop });
+
+  const style = useMemo(
+    () => ({
+      ...['base'],
+      ...(isFocused ? ['base focused'] : {}),
+      ...(isDragAccept ? ['base accept'] : {}),
+      ...(isDragReject ? ['base reject'] : {}),
+    }),
+    [isFocused, isDragAccept, isDragReject]
+  );
+
   return (
     <main>
       <title>Home Page</title>
       <h1>Hello world</h1>
 
       <div className="container">
-        <div className="columns">
-          <div className="column">
-            <h2 className="title is-2">Level 2 heading</h2>
-            <p className="content">Cool content. Using Bulma!</p>
-          </div>
-          <div className="column is-four-fifths">
-            <h2 className="title is-2">Level 2 heading</h2>
-            <p className="content">This column is cool too!</p>
+        <div className="column">
+          <div className="dndinput">
+            <div {...getRootProps()} className={style[0]}>
+              <input {...getInputProps()} />
+              <p>Drag 'n' drop some files here, or click to select files</p>
+            </div>
           </div>
         </div>
       </div>
