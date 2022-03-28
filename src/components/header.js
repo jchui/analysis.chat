e41253846390e51a6b043fa69bb05b08b389777a
Jeremy Chui <jeremy@jchui.me>
2022-03-28 18:11:19 +0100

    Added layout + header components

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import '../styles/style.scss';
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">Hello</div>
+    </>
+  );
+};
+
+export default Header;
