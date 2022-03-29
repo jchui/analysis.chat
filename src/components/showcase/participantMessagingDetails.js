d05aa2766c610f22da542725bdcda512048ed4a1
Jeremy Chui <jeremy@jchui.me>
2022-03-29 06:05:48 +0100

    Updated participantMessagingDetails

--- src/components/showcase/participantMessagingDetails.js
+++ src/components/showcase/participantMessagingDetails.js
@@ -1,32 +1,37 @@
 import React from 'react';
-import Anime, { anime } from 'react-anime';
 
-const ParticipantMessagingDetails = () => {
-
+const ParticipantMessagingDetails = ({
+  chatFirstMessageDate: chatFirstMessageDate,
+  chatAvgWeeklyMessages: chatAvgWeeklyMessages,
+  chatLongestDayStreak: chatLongestDayStreak,
+}) => {
   return (
     <>
-        <div className="whiteDetailsBanner">
-          <div className="columns">
-            <div className="column is-one-third">
-              <p>
-                <small>You started talking on</small><br/>
-                September 27th, 2021
-              </p>
-            </div>
-            <div className="column is-one-third">
-              <p>
-                <small>Since then, you've sent each other</small><br/>
-                203 messages a week
-              </p>
-            </div>
-            <div className="column is-one-third">
-              <p>
-                <small>Your longest messaging streak is</small><br/>
-                21 days
-              </p>
-            </div>
+      <div className="whiteDetailsBanner">
+        <div className="columns">
+          <div className="column is-one-third">
+            <p>
+              <small>You started talking on</small>
+              <br />
+              {chatFirstMessageDate}
+            </p>
+          </div>
+          <div className="column is-one-third">
+            <p>
+              <small>Since then, you've sent each other</small>
+              <br />
+              {chatAvgWeeklyMessages} messages a week
+            </p>
+          </div>
+          <div className="column is-one-third">
+            <p>
+              <small>Your longest messaging streak is</small>
+              <br />
+              {chatLongestDayStreak} days
+            </p>
           </div>
         </div>
+      </div>
     </>
   );
 };
