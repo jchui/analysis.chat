785fcf7aa53f7d84c28871ca9a4b2e694ad53cd3
Jeremy Chui <jeremy@jchui.me>
2022-03-31 18:46:57 +0100

    Added chatLog parsing

--- src/components/functions.js
+++ src/components/functions.js
@@ -5,6 +5,7 @@ const parseAcceptedFile = (acceptedFile) => {
 
     let output = { name: null, files: [], chatLog: [] };
     let fileContent = [];
+    let chatLog = [];
 
     var title = acceptedFile[0].name;
     title = title.replace('WhatsApp Chat - ', '');
@@ -12,17 +13,21 @@ const parseAcceptedFile = (acceptedFile) => {
     
     JSZip.loadAsync(acceptedFile[0])                                   // 1) read the Blob
         .then(function(zip) {
-            var dateAfter = new Date();
 
             zip.forEach(function (relativePath, zipEntry) {  // 2) print entries
                 fileContent.push(zipEntry);
             });
+
+            chatLog = zip.file('_chat.txt');
+
         }, function (e) {
             console.log(acceptedFile[0].name + ': ' + e.message);
         });
+        
 
     output.name = title;
     output.files = fileContent;
+    output.chatLog = chatLog;
 
     return output;
 
